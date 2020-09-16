f774a717a168e7fcb8816df6d6f91ea950ca76ec
Alvaro Galarza <alvarogalarzaalmanza@gmail.com>
2020-09-16 11:28:11 -0400

    m

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
-// let {
-//   more : 'More',
-//   "integrantes"
-// }
+var integrantes = [
+  { name: "Alvaro Mauricio", lastname: "Galarza Almanza", codigo: "C7474-8" },
+  { name: "Dayana Stefka", lastname: "Ledezma Bohorquez", codigo: "A19083-7" },
+  { name: "Morelia", lastname: "Hidalgo Caceres", codigo: "C7504-3" }
+];
+
 app.get('/',function(req,res) {
-  res.send("Hello World");
+  res.send(integrantes);
 });
 
 app.listen(4000);
